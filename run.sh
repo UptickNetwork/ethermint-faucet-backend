ed507e6d525426afbeba3b23ebdd6123f79f5465
jiuyunyue <bert.kukoo@gmail.com>
2022-03-18 13:59:49 +0800

    change start way

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ export POSTGRES_PORT=5432
 export POSTGRES_DB=faucet
 export PROM_USER=postgres
 export PROM_PASSWORD=test
-export NETWORK_RPC_NODE="http://127.0.0.1:26657"
+export NETWORK_RPC_NODE="http://peer1.testnet.uptick.network:26657"
 export FAUCET_WAIT_PERIOD=1d
 export FAUCET_DISTRIBUTION_AMOUNT=10000000000000000
 export FAUCET_DENOM=auptick
@@ -14,11 +14,12 @@ export FAUCET_MEMO="send amount"
 export ADDRESS_PREFIX="uptick"
 export AUTH0_DOMAIN="uptick-faucet.jp.auth0.com";
 export AUTH0_AUDIENCE="http://zhangboxing.com";
-export FAUCET_MNEMONIC="visual trend scene cake abstract farm fee orchard child deposit power mean capable chalk enough orange make vendor direct enlist pill dragon early autumn"
+export FAUCET_MNEMONIC=""
 
 bash supportEthermint.sh
 bash supportUptick.sh
 docker-compose  -f db-compose.yml up -d
 yarn migrate
-yarn start
-#
+
+mkdir logs
+nohup yarn start > ./logs/faucet.log 2>&1 & 
